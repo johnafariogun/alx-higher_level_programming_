96c305a930d65970e78e2bb1a8a2509e786b2511
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-12-05 11:19:32 -0800

    learning

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    list_ = []
+    for i in my_list:
+        if i % 2 == 0:
+            list_.append(True)
+        else:
+            list_.append(False)
+    return list_
