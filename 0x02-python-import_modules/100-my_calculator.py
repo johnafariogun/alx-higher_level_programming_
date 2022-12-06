474651c2c05c119018f5c301f5737569b4fab008
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-12-06 06:17:29 -0800

    learning

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,22 +3,26 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv, exit
-
-    if len(argv) - 1 != 3:
+    argv = argv[1:]
+    argc = len(argv)
+    if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    #this is a dictionary of the operator as the key and function as the value
-    operators = {
-            '+': add,
-            '-': sub,
-            '*': mul,
-            '/': div
-            }
-    #checks if the operator isn't in the list of current operators
-    if argv[2] not in list(operators.keys()):
+
+    i = int(argv[0])
+    j = int(argv[2])
+    operator = argv[1]
+
+    if operator == '+':
+        result = add(i, j)
+    elif operator == '-':
+        result = sub(i, j)
+    elif operator == '*':
+        result = mul(i, j)
+    elif operator == '/':
+        result = div(i, j)
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
-    i = int(argv[1])
-    j = int(argv[3])
-    print("{} {} {} = {}".format(i, argv[2], j, operators[argv[2]](i, j)))
+    print("{} {} {} = {}".format(i, operator, j, result))
