efcdd45c338407d31f5b0f0dfec9fa19986b0ea7
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2023-01-12 09:37:22 -0800

    lea

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,7 @@
 checks if an object is exactly the same with a class
 """
 
+
 def is_same_class(obj, a_class):
     """exactly the same return True"""
     return (type(obj) == a_class)
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,6 +3,7 @@
 initializes a module(function)
 """
 
+
 def is_kind_of_class(obj, a_class):
     """function that returns true if obj is instance of class or inherits"""
     return isinstance(obj, a_class)
