68d72694d59c8379cf09eaef8584d467588ca07a
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2023-01-14 17:34:09 -0800

    done with mandatory

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""creating a subclass that inherits from Rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square"""
+
+    
+    def __init__(self, size):
+        """initial attributes and declarations"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+        self.area()
+
+    def __str__(self):
+        """contains info about the class"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
