6538f260335648aa0fad6966c3709bdeb5ac2abf
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2023-01-14 18:06:54 -0800

    having checker error

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,14 @@
 
 
 def add_attribute(obj, name, value):
+    """adds a new attribute to an object if possible
+        Args:
+            obj : the object, can be of any data type
+            name: name of the attribute (str)
+            value: value at the name (any)
+        Raise:
+            TypeError: if the attribute can't be added
+    """
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     obj.__setattr__(name, value)
