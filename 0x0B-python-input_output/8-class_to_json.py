e54cdcf39ec7e1d41ef792cb87d84c03add2fa1d
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2023-01-27 02:27:23 -0800

    i am learning

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines a function that returns a dictionary description with simple 
+data structure (list, dictionary, string, integer and boolean) for
+JSON serialization of an object"""
+
+
+
+def class_to_json(obj):
+    """returns the dixtionary representation of a class"""
+    return obj.__dict__
