862934ee036d0d6535554ab221d9455744431142
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2023-01-27 03:00:07 -0800

    i am learning

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" creates a function to create a pscal triangle"""
+
+
+def pascal_triangle(n):
+    """function that returns a list of numbers represernting
+    a pascal triangle
+    args:
+        n: integer which is the length of pascal
+    """
+
+    if n <= 0:
+        return []
+
+    tri  = [[1]]
+
+    for i in range(1,n):
+        line = [1]
+        for j in range(1, i):
+            line.append(tri[i-1][j-1] + tri[i-1][j])
+        line.append(1)
+        tri.append(line)
+    return tri
