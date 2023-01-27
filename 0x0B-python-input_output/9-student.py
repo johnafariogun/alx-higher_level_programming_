2758649bf0d4524928d599548486ce3a1aa74bfd
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2023-01-27 02:40:29 -0800

    i am learning

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,7 +4,7 @@
 
 class Student:
     """creates a class student with different public attributes"""
-    
+
     def __init__(self, first_name, last_name, age):
         """
         args:
@@ -15,7 +15,6 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
 
     def to_json(self):
         """returns the dixtionary representation of a class"""
