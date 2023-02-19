dc8181a3a35516353242c1efed39a40baeb4d7a1
Afariogun John Tolulope <afariogunjohn2502@gmail.com>
2023-02-19 07:36:33 -0800

    this is me testing if my code works

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- lists all sbhows contained in hbtn_0d_tvshows nwhich have at least one genre linked sorted by ascending tvshows title and genre_id
-SELECT serie.`title`, genre.`genre_id` FROM `tv_shows` AS serie INNER JOIN `tv_shows_genres` AS genre ON serie.`id` = genre.`show_id` ORDER BY serie.`title`, genre.`genre_id`;
+SELECT serie.`title`, genre.`genre_id` FROM `tv_shows` INNER JOIN `tv_shows_genres` ON serie.`id` = genre.`show_id` ORDER BY serie.`title`, genre.`genre_id`;
