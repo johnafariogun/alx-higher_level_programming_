c401c5ac213b151a37f3e81f8ecfa729220145ed
Afariogun John Tolulope <afariogunjohn2502@gmail.com>
2023-02-19 04:08:37 -0800

    i am not sure if this is correct cause of i set value to strictly not NULL

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- this creates a table force_name with a default id of 1
+CREATE TABLE IF NOT EXISTS `id_not_null`(`id` INT DEFAULT 1, `name` VARCHAR(256) NOT NULL);
--- 0x0E-SQL_more_queries/test.sh
+++ 0x0E-SQL_more_queries/test.sh
@@ -1,5 +1,9 @@
-#!/usr/bin/bash
-read e
+#!/usr/bin/env bash
+# this is a shell script that helps me simplify adding and pushing to github
+# Usage:
+#	echo "whatever commit message you want to use" | ./test.sh
+# your commit message is read as an input and then all files are added, commited and pushed to your current remote branch on the repository
+read commit_message
 git add .
-git commit -m "$e"
+git commit -m "$commit_message"
 git push
