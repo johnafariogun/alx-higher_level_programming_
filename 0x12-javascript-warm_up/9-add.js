d1751ad697c6b810fa5abb786280904f5e31737c
John Tolulope Afariogun <afariogunjohn2502@gmail.com>
2023-03-14 00:02:42 +0000

    fixed checker error

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 const x = Math.floor(Number(process.argv[2]));
 const y = Math.floor(Number(process.argv[3]));
 if (x && y) {
-    console.log(x + y);
+  console.log(x + y);
 } else {
   console.log('NaN');
 }
