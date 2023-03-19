13dcbfff0cdaa4ddd955a8f9130889e9514cbe4c
Afariogun John Tolulope <afariogunjohn2502@gmail.com>
2023-03-19 01:10:17 -0700

    i am not really sure if i set the right encoding

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+this module declares a state class which inherits a Base instance to be mapped to a MySQL database table
+"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+        This simply represents a MySQL database
+
+        __tablename__ (str): the name of the MySQL table to store the States
+        id (sqlalchemy.Integer): state id
+        name (sqlalchemy.String): state name
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
