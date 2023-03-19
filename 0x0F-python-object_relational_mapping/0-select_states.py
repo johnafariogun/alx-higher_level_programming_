f7b27129333f091e0234d6acaa236810fe2bbfea
Afariogun John Tolulope <afariogunjohn2502@gmail.com>
2023-03-19 00:13:45 -0700

    i have done the first two tasks but i want to confirm if my query should be writte in the python expression or the sql query

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this lists all states from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    connect = db.cursor()
+    connect.execute("SELECT * FROM `states`")
+    states = connect.fetchall()
+    [print(state) for state in states]
