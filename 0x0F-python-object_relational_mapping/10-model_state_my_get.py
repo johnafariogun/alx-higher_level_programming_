c65d5117f6dc3e1367816ce214610057794c4ca8
Afariogun John Tolulope <afariogunjohn2502@gmail.com>
2023-03-19 01:29:50 -0700

    i want to test with the checker before refactoring the code

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    lists State objects that have the name passed as argument
+    Usage: ./7-model_state_fetch_all.py <mysql username> \
+                                        <mysql password> \
+                                        <database name> \
+                                        <state name>
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    name = sys.argv[4]
+
+    for state in session.query(State).filter_by(name=name).order_by(State.id):
+        print("{}".format(state.id))
