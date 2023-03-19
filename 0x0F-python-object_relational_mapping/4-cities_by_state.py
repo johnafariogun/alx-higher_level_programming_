2afdaca0370280daf2e8a38f8f578a8e936fe579
Afariogun John Tolulope <afariogunjohn2502@gmail.com>
2023-03-19 00:44:01 -0700

    checking out task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+this lists all states from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    connect = db.cursor()
+    connect.execute("SELECT * FROM `cities` as city INNER JOIN `states` as\
+            `state` ON city.state_id = state.id ORDER BY city.id")
+    cities = connect.fetchall()
+    [print(city) for city in cities]
