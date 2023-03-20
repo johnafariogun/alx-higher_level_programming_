ae7cc538d9dfa65720e0a5f69dbb61e3470c2a20
Afariogun John Tolulope <afariogunjohn2502@gmail.com>
2023-03-20 12:18:23 -0700

    learning pycodestyle funny rules

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,7 +8,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     connect = db.cursor()
-    connect.execute("SELECT * FROM `citys` WHERE `name` = %s ORDER BY `id`",\
-            (sys.argv[4], ))
+    connect.execute("SELECT * FROM `citys` WHERE `name` = %s ORDER BY `id`",
+                    (sys.argv[4], ))
     states = connect.fetchall()
     [print(state) for state in states]
