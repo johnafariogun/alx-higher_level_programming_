a83afb334576e46cc5fdac68c27a9bb261c35df3
Afariogun John Tolulope <afariogunjohn2502@gmail.com>
2023-03-20 12:13:45 -0700

    i think i have solved most of the checker errors

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -17,7 +17,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State):
-        if 'a' in state.name:
-            session.delete(state)
+    for state in session.query(State).filter(State.name.contains('a')).all():
+        session.delete(state)
     session.commit()
