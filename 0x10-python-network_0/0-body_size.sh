45168a5693c3a607df8361fb83efc48b024e6e20
Afariogun John Tolulope <afariogunjohn2502@gmail.com>
2023-04-17 10:12:16 -0700

    i dont know whats wrong with the checker

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # takes in a URL, sends a request to it and returns the response length
-curl -s "$1" | wc -c
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
