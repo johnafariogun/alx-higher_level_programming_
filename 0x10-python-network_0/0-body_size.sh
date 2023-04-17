4db9db804619f9ee7fd07559666e285369d9265b
Afariogun John Tolulope <afariogunjohn2502@gmail.com>
2023-04-17 10:07:41 -0700

    I had to make the server listen on port 5000

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# takes in a URL, sends a request to it and displays the size of the body of the response
+# takes in a URL, sends a request to it and returns the response length
 curl -s "$1" | wc -c
