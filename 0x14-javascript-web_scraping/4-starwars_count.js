dcdc96a644f7fc53df6121decf8e7e31c7a5862e
Afariogun John Tolulope <afariogunjohn2502@gmail.com>
2023-04-26 07:01:42 -0700

    testng

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const url = process.argv[2]
 
 request(url, function (error, response, body) {
   if (error) { console.log(error); } else if (response.statusCode === 200) {
-    const responseJson = JSON.parse(body);
-    console.log(responseJson.title);
+    const result = JSON.parse(body).results;
+	  let count = 0;
+    console.log(result);
   } else { console.log('Error code: ' + response.statusCode); }
 });
