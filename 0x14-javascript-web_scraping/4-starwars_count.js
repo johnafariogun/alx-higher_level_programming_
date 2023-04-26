f24cf72de51c5d747621ec1ce9502c8b75e020a8
Afariogun John Tolulope <afariogunjohn2502@gmail.com>
2023-04-26 10:40:28 -0700

    finally done with the mandatroy tasks, moving on to the optional

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2]
+const url = process.argv[2];
 
 request(url, function (error, response, body) {
-  if (error) { console.log(error); } else if (response.statusCode === 200) {
+  if (response.statusCode === 200) {
     const result = JSON.parse(body).results;
-	  let count = 0;
-    console.log(result);
-  } else { console.log('Error code: ' + response.statusCode); }
+    let count = 0;
+    for (const index in result) {
+      const film = result[index].characters;
+      for (const charIndex in film) {
+        if (film[charIndex].includes('18')) { count++; }
+      }
+    }console.log(count);
+  } else if (error) { console.log(error); } else { console.log('Error code: ' + response.statusCode); }
 });
