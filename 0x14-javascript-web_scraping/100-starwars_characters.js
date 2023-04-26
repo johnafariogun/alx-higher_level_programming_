d7203a8953ce34f65b5c6df52d0f67dc53895cb4
Afariogun John Tolulope <afariogunjohn2502@gmail.com>
2023-04-26 11:11:21 -0700

    fixed the errors

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,16 +1,22 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
 
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
-      });
+const request = require('request');
+const url = 'https://swapi.dev/api/films/' + process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const characters = JSON.parse(body).characters;
+
+  for (const character of characters) {
+    request(character, (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      console.log(JSON.parse(body).name);
     });
   }
 });
